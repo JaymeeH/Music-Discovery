0246dc80bfc56ac0429e59b07be6e624fc66474c
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-02-14 00:15:03 +0000

    code cleanup

--- app1.py
+++ app1.py
@@ -26,6 +26,6 @@ def hello_world():
 
 if __name__ == "__main__":
     app1.run(
-        port=int(os.getenv('PORT',8080)),
-        host=os.getenv('IP' '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        host=os.getenv('IP', '0.0.0.0'),
 )
